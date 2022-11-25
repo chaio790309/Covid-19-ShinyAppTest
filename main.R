1d0419f610bac2ed4a9e6181c8fb2c9a988c42b7
Milkshake <chaio790309@gmail.com>
2022-11-25 18:02:26 +0800

    total + datatable + brush

--- main.R
+++ main.R
@@ -1,52 +1,82 @@
 library(shiny)
 library(dplyr)
 library(ggplot2)
+library(plotly)
 
 
-r<-read.csv(file="https://od.cdc.gov.tw/eic/covid19/covid19_tw_specimen.csv")
-r<-r[,c(1,5)]
-names(r)[1]<-list("Date")
+data<-read.csv(file="https://covid-19.nchc.org.tw/api/csv?CK=covid-19@nchc.org.tw&querydata=4051&limited=TWN",header=TRUE,sep=",",fileEncoding = "big5")
+data<-data[,c(5:7)]
+names(data)<-list("Date","Total","Num")
 #日期格式為文字(x軸順序會亂掉)，轉為日期
-r$Date<-as.Date(r$Date)
+data$Date<-as.Date(data$Date)
 #三個新欄位給均線用
-r$weekavg<-array(0,nrow(r))
-r$monthavg<-array(0,nrow(r))
-r$quarteravg<-array(0,nrow(r))
+data$weekavg<-array(0,nrow(data))
+data$monthavg<-array(0,nrow(data))
+data$quarteravg<-array(0,nrow(data))
 #計算各平均值
-for(i in 7:nrow(r)){
-  r$weekavg[i]<-mean(r$Total[(i-7+1):i])
+for(i in 7:nrow(data)){
+  data$weekavg[i]<-mean(data$Num[(i-7+1):i])
 }
-for(i in 30:nrow(r)){
-  r$monthavg[i]<-mean(r$Total[(i-30+1):i])
+for(i in 30:nrow(data)){
+  data$monthavg[i]<-mean(data$Num[(i-30+1):i])
 }
-for(i in 90:nrow(r)){
-  r$quarteravg[i]<-mean(r$Total[(i-90+1):i])
+for(i in 90:nrow(data)){
+  data$quarteravg[i]<-mean(data$Num[(i-90+1):i])
 }
 
 ui <- fluidPage(
+  titlePanel("Practive Shimy"),
   sidebarLayout(
     sidebarPanel(
-      dateRangeInput("daterange","Select date range","2022-05-01",Sys.Date()-1,min="2020-01-15",max=Sys.Date()-1),
+      dateRangeInput("daterange","Select date range","2022-09-01",Sys.Date()-1,min="2022-01-01",max=Sys.Date()-1),
       radioButtons("cycle","Select Moving Average days",list("weekavg","monthavg","quarteravg"),"weekavg")
     ),
     mainPanel(
-      plotOutput("plot")
+      tabsetPanel(
+        tabPanel(
+          "Days",
+          plotOutput("plot",brush = "plot_brush"),
+          dataTableOutput("table"),
+          verbatimTextOutput("test")
+          ),
+        tabPanel(
+          "Total",
+          
+        )
+      )
+
     )
   )
 )
 
 server <- function(input, output, session) {
+  
 
   datasubset <- reactive(
-    r %>% filter(Date >= input$daterange[1] & Date <= input$daterange[2])
+    data %>% filter(Date >= input$daterange[1] & Date <= input$daterange[2])
     )
   output$plot<-renderPlot(
     ggplot(datasubset())+
-      geom_line(aes(Date,Total,group=1,color="Total"))+
-      geom_line(aes_string("Date",input$cycle,group=1),color="purple")+
-      scale_colour_manual(name="每日與均線",values = c("Total"="blue",Average="purple"))
-    
+      geom_bar(aes(Date,Num), stat = "identity",fill="#00BBFF")+
+      geom_line(aes_string("Date",input$cycle,group=1),color="purple")
+      #scale_colour_manual(name="每日與均線",values = c(Average="purple"))
   )
+
+  output$test<-renderText(
+    if(max(input$daterange[1],input$plot_brush[[1]])==input$daterange[1]){
+      input$daterange[1]
+    }else{
+      input$plot_brush[[1]]
+    }
+  )
+
+  output$table<-renderDataTable(
+    brushedPoints(data,input$plot_brush)
+    #data %>% filter(Date >= input$plot_brush$xmin & Date <= input$plot_brush$xmax)
+
+  )
+
+  
 }
 
 shinyApp(ui, server)
